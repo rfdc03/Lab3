989f746f7c0592e7ae38e2f8c12477da3ea16e6d
Rodrigo Dulanto <>
2020-05-07 12:02:07 -0500

    Se crea el proyecto.

---
+++ CongreSoft/src/pe/edu/pucp/congresoft/model/Congreso.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.congresoft.model;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Congreso {
+    private int id;
+    private String nombre;
+    //private ;FECHA_INICIO
+    //pRIVATE FECHA FIN
+    private String pais;
+    private int activo;
+    
+}
