378f6f4f998673cad0c1a371e1a234f9d1238894
Rodrigo Dulanto <Rodrigo@DESKTOP-TFIE6KL>
2020-05-07 12:37:06 -0500

    Avance del proyecto.

--- CongreSoft/src/pe/edu/pucp/congresoft/model/Congreso.java
+++ CongreSoft/src/pe/edu/pucp/congresoft/model/Congreso.java
@@ -5,6 +5,8 @@
  */
 package pe.edu.pucp.congresoft.model;
 
+import java.util.Date;
+
 /**
  *
  * @author Rodrigo
@@ -12,9 +14,49 @@ package pe.edu.pucp.congresoft.model;
 public class Congreso {
     private int id;
     private String nombre;
-    //private ;FECHA_INICIO
-    //pRIVATE FECHA FIN
+    private Date fecha_inicio;
+    private Date fecha_fin;
     private String pais;
     private int activo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public Date getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
     
 }
---
+++ CongreSoft/src/pe/edu/pucp/congresoft/mysql/CongresoMySQL.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.congresoft.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.edu.pucp.congresoft.dao.CongresoDAO;
+import pe.edu.pucp.congresoft.model.Congreso;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class CongresoMySQL implements CongresoDAO {
+
+    @Override
+    public int insertar(Congreso congreso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Congreso> listar() {
+        ArrayList<Congreso> congresos = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://"+
+                    "lp2mysql.cdjj2wtonimd.us-east-1.rds.amazonaws.com"+
+                    ":3306/inf282", "admin", "abcd1234");
+            String sentencia = "SELECT * FROM EMPLEADO";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sentencia);
+            while(rs.next()){
+                Congreso congreso = new Congreso();
+                congreso.setId(rs.getInt("ID_CONGRESO"));
+                congreso.setNombre(rs.getString("NOMBRE"));
+                congreso.setPais(rs.getString("PAIS"));
+                congreso.setFecha_inicio(rs.getDate("FECHA_INICIO"));
+                congreso.setFecha_fin(rs.getDate("FECHA_FIN"));
+                
+            
+            }
+            con.close();
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return congresos;
+    }
+    
+}
